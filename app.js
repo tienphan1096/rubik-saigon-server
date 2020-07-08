574846652404c5634657d2a5cd9dc1be20c444f1
Tien Phan <tien@routeique.com>
2020-07-08 17:58:14 -0600

    Initial commit.

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+var router = require('./routes/index')
+
+app.use('/', router)
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
