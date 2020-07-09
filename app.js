340df8674f807ed2ee5078e01f9db896b9172116
Tien Phan <tien@routeique.com>
2020-07-08 19:29:00 -0600

    Sample image resizing.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = 3000
 
 var router = require('./routes/index')
 
+app.use(express.static('public'))
 app.use('/', router)
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var sharp = require('sharp')
 
 router.get('/scan', function(req, res, next) {
-  res.send('respond with a resource');
+    sharp('public/images/main/20181116_000204.jpg')
+    .rotate()
+    .resize(445, 445, {
+        fit: 'inside'
+    })
+    .toFile('public/images/thumbnails/20181116_000204_2.jpg', err => {
+        console.log(err)
+        res.send('respond with a resource');
+    })
 });
 
 module.exports = router;
