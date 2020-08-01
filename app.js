1dacabad5ec3fddddee3250408e5148b891fc3ec
Tien Phan <tien@routeique.com>
2020-07-31 18:31:18 -0600

    Enable HTTPS on prod.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
 const express = require('express')
+const https = require('https')
+const fs = require('fs');
 const app = express()
 const port = 3000
 
@@ -15,4 +17,11 @@ const router = require('./routes/index')
 app.use(express.static('public'))
 app.use('/', router)
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+if (process.env.NODE_ENV === 'production') {
+  const privateKey  = fs.readFileSync('keys/key.pem')
+const certificate = fs.readFileSync('keys/cert.pem')
+const bundle = fs.readFileSync('keys/bundle.ca-bundle')
+  https.createServer({key: privateKey, cert: certificate, ca: bundle}, app).listen(port)
+} else {
+  app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+}
